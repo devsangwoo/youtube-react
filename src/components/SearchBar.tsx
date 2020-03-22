e244be7b38aecf8e8286b65f35e15f630989af60
sangwoo <dev.sangwoo@gmail.com>
2020-03-22 21:42:00 +0900

    [react]: add input handler

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import search from 'youtube-search';
 
@@ -8,14 +8,32 @@ const opts = {
   maxResults: 10,
   key: 'AIzaSyCUV-L_Hku4Y-tUSWpybhavgNWbb_aGS6M',
 };
+
 const SearchBar = (props: Props) => {
-  search('surf', opts, (err, results) => {
-    if (err) return console.log(err);
+  const initialState = { term: '' };
+  const [searchTerm, setSearchTerm] = useState(initialState);
+  const { term } = searchTerm;
+  const onInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    //setSearchTerm은  state[name] = value와 같음
+    //객체가 복사되면서 e.target.value의 value가 searchTerm.term이 됨
+    //[name]: value  =>e.target.name과 같은 요소에 value를 할당해라.
+    //인풋의 name에 해당하는 요소에 스테이트로서 벨류값을 할당함
+    setSearchTerm({ ...searchTerm, [name]: value });
+    console.log(searchTerm);
+    // // search('searchTerm', opts, (err, results) => {
+    // //   if (err) return console.log(err);
 
-    console.log(results);
-  });
+    // //   console.log(results);
+    // });
+  };
 
-  return <div>SearchBar</div>;
+  return (
+    <div>
+      SearchBar
+      <input name={'term'} onChange={term => onInputHandler(term)} value={term} />
+    </div>
+  );
 };
 
 export default SearchBar;
